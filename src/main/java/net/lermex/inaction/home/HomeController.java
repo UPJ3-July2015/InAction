d38df472583145ccdfa6d1762a07d039c91c832a
Nick <mykola.veremeichyk@gmail.com>
2015-08-26 21:00:42 +0300

    new graph

--- src/main/java/net/lermex/inaction/home/HomeController.java
+++ src/main/java/net/lermex/inaction/home/HomeController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/")
 	public ModelAndView index() {
+		List<Integer> listOfIntegers = Arrays.asList(4, 5, 6, 12, 3, 4, 5);
         final ModelAndView mav = new ModelAndView("home/homeNotSignedIn");
-        mav.addObject("customers", "test");
+		mav.addObject("listOfIntegers", listOfIntegers);
+
 		return mav;
 	}
 
