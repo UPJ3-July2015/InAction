98d62530efecfedbb1f566020597d3d07c60e70b
unknown <omoniato@HORNOTE-NZLN-3.astelit.ukr>
2015-08-26 21:02:01 +0300

    Add activity minutes to controller

--- src/main/java/net/lermex/inaction/home/HomeController.java
+++ src/main/java/net/lermex/inaction/home/HomeController.java
@@ -3,6 +3,8 @@ package net.lermex.inaction.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -10,7 +12,9 @@ public class HomeController {
 	@RequestMapping(value = "/")
 	public ModelAndView index() {
         final ModelAndView mav = new ModelAndView("home/homeNotSignedIn");
+		List<Integer> activityMinutes = Arrays.asList(0,2,5,3,4,5,6);
         mav.addObject("customers", "test");
+        mav.addObject("activityMinutes", activityMinutes);
 		return mav;
 	}
 
